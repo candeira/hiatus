c85638354a186946d27f5629c9e96f1512836b1c
Joshua Holbrook <josh.holbrook@gmail.com>
2011-07-17 22:53:30 -0700

    Threaded version. Works okay.

--- hiatus.py
+++ hiatus.py
@@ -1,28 +1,36 @@
-import signal
+from threading import Timer
 import types
 
-def note():
-    print 'IM MAKING A NOTE HERE'
-
-def set_interval(f, time=None):
-    def _set_interval(f, time):
-        def handler(signum, frame):
-            f()
-
-        signal.signal(signal.SIGALRM, handler)
-        signal.setitimer(signal.ITIMER_REAL, time/1000.0, time/1000.0)
+def set_timeout(f, time=None):
+    def _set_timeout(f, time):
+        interval = Timer(time, f)
+        interval.start()
+        return interval
 
     if time==None:
         #f is actually time
-        return lambda g: _set_interval(g, f)
+        return lambda g: _set_timeout(g, f)
     else:
-        _set_interval(f, time)
+        return _set_timeout(f, time)
 
-set_interval(note, 500)
+def set_interval(f, time=None):
+    def again(g, t):
+        def _again():
+            set_timeout(again(g, t), t)
+            g()
+        return _again
 
-@set_interval(1000)
+    if time==None:
+        return lambda g: set_timeout(again(g, f), f)
+    else:
+        set_timeout(again(f, time), time)
+
+
+@set_interval(1.000)
 def success():
     print "HUGE SUCCESS"
 
-while(True):
-    pass
+def note():
+    print "I'M MAKING A NOTE HERE:"
+
+set_timeout(note, 0.500)
