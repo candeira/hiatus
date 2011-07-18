2e6094374d8d17b56cc4642d34070c02dec04973
Joshua Holbrook <josh.holbrook@gmail.com>
2011-07-17 23:31:58 -0700

    Working timeout action.

--- hiatus.py
+++ hiatus.py
@@ -1,36 +1,48 @@
 from threading import Timer
-import types
 
-def set_timeout(f, time=None):
-    def _set_timeout(f, time):
-        interval = Timer(time, f)
-        interval.start()
-        return interval
+class set_timeout(object):
+    def __init__(self, f, time=None):
+        self._time = None
 
-    if time==None:
-        #f is actually time
-        return lambda g: _set_timeout(g, f)
-    else:
-        return _set_timeout(f, time)
+        if time==None:
+            #f is actually time
+            self._time = f
+        else:
+            self._set_timeout(f, time)
 
-def set_interval(f, time=None):
-    def again(g, t):
-        def _again():
-            set_timeout(again(g, t), t)
-            g()
-        return _again
+    #For the decorators.
+    def __call__(self, g):
+        self._set_timeout(g, self._time)
 
-    if time==None:
-        return lambda g: set_timeout(again(g, f), f)
-    else:
-        set_timeout(again(f, time), time)
+    def _set_timeout(self, f, time):
+        self._timeout = Timer(time, f)
+        self._timeout.start()
+
+def clear_timeout(timeout_obj):
+    timeout_obj._timeout.cancel()
 
 
-@set_interval(1.000)
-def success():
-    print "HUGE SUCCESS"
+class set_interval(object):
+    def __init__(self, f, time=None):
+        self._time = None
+        if time==None:
+            self._time = f
+        else:
+            self._set_interval(f, time)
 
-def note():
-    print "I'M MAKING A NOTE HERE:"
+    #For the decorators.
+    def __call__(self, g):
+        self._set_interval(g, self._time)
 
-set_timeout(note, 0.500)
+    def _set_interval(self, f, time):
+        def again(g, t):
+            def _again():
+                g()
+                self._set_interval(g, t)
+            return _again
+
+        self._interval = Timer(time, again(f, time))
+        self._interval.start()
+
+def clear_interval(interval_obj):
+    interval_obj._interval.cancel()
